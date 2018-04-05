5cf56f23f199411eed6e8af55ffb21c57535597c
corselia <takiya@toran.sakura.ne.jp>
2018-04-05 19:21:15 +0900

    [update] update sample filters.rb
    update readable method is alive, unreadable method is comment out

--- config/filters.rb
+++ config/filters.rb
@@ -1,7 +1,7 @@
 module Filters
   def chrome_or_fx
-    filter :chrome_or_fx, &lambda { ['Chrome', 'Firefox'].map {|browser| matches(:browser, browser)} }
-    # filter :chrome_or_fx, &lambda { contains(:browser, 'Chrome|Firefox') }
+    filter :chrome_or_fx, &lambda { contains(:browser, 'Chrome|Firefox') }
+    # filter :chrome_or_fx, &lambda { ['Chrome', 'Firefox'].map { |browser| matches(:browser, browser) } }
   end
 
   def over_one_thousand_search_sessions
